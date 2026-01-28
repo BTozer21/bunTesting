72dcc3d73fb35534b99fbf419a8f126bf06be411
BTozer21 <benji.tozer@icloud.com>
2026-01-28 18:28:08 +0000

    Adding links for the restaurant menu and reviews

--- src/Battersea.tsx
+++ src/Battersea.tsx
@@ -9,7 +9,9 @@ export function Battersea() {
     <div className="container flex flex-row mx-auto p-8 items-center justify-center text-center relative z-10 h-[calc(100vh-4rem)]">
        <Card className="mx-auto w-[650px]">
         <CardHeader className="gap-4">
-			<img className="rounded-sm h-[400px] w-[600px] object-cover" src="https://www.gordonramsayrestaurants.com/assets/Uploads/grr-battersea.jpg" />
+			<a className="hover:shadow-lg transition-all duration-300 hover:scale-101" href="https://www.gordonramsayrestaurants.com/assets/Uploads/MenuPDFImage/pdf-preview-BSK-BATTERSEA-ALC-WINTER-20260.png">
+				<img className="rounded-sm h-[400px] w-[600px] object-cover" src="https://www.gordonramsayrestaurants.com/assets/Uploads/grr-battersea.jpg" />
+			</a>
 			<CardTitle className="gap-2">
 				<div className="text-3xl font-bold text-left">
 					Gordon Ramsay Bread Street
@@ -21,9 +23,11 @@ export function Battersea() {
 						<Star fill="gold" color="gold"/>
 						<Star fill="gold" color="gold"/>
 					</div>
-					<div className="text-xl">
-						(1.6k Reviews)
-					</div>
+					<a className="hover:underline" href="https://www.google.com/search?client=firefox-b-d&hs=IAaU&sca_esv=a675286474cf8d02&sxsrf=ANbL-n6dJHX7-olr8MixGwP4b0HU8pJg3g:1769623577507&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOUxIBdS4IPzHrPFpw2QU_DKOURCbYGldkAhG9C2dIYfTUddVS4HNqROjbR0QDHOO3LcPu0aQzRcqMswOAHovEiHJ3BMgcFOSMQJAI-6B0GbZ-KWlXs9zDIWUIjZJ1w3waAqkyT0%3D&q=Bread+Street+Kitchen+%26+Bar+-+Battersea+Reviews&sa=X&ved=2ahUKEwiE0eTF6a6SAxW-UUEAHdwZB_sQ0bkNegQIIBAH&biw=1464&bih=1186&dpr=1&aic=0">
+						<div className="text-xl">
+							(1.6k Reviews)
+						</div>
+					</a>
 				</div>
 			</CardTitle>
 			<CardDescription className="text-2xl font-semibold text-left">
--- src/CoventGarden.tsx
+++ src/CoventGarden.tsx
@@ -9,7 +9,9 @@ export function CoventGarden() {
     <div className="container flex flex-row mx-auto p-8 items-center justify-center text-center relative z-10 h-[calc(100vh-4rem)]">
        <Card className="mx-auto w-[650px]">
         <CardHeader className="gap-2">
-			<img className="rounded-sm h-[400px] w-[600px] object-cover" src="https://www.fattoamanopizza.com/wp-content/uploads/2024/10/Locations_CG_FattoaManoPizza-01.jpg" />
+			<a className="hover:shadow-lg transition-all duration-300 hover:scale-101" href="https://www.fattoamanopizza.com/wp-content/uploads/2025/09/LONDON-SCREEN.pdf">
+				<img className="rounded-sm h-[400px] w-[600px] object-cover" src="https://www.fattoamanopizza.com/wp-content/uploads/2024/10/Locations_CG_FattoaManoPizza-01.jpg" />
+			</a>
 			<CardTitle className="gap-2">
 				<div className="text-3xl font-bold text-left">
 					Fatto a Mano
@@ -22,9 +24,11 @@ export function CoventGarden() {
 						<Star fill="gold" color="gold"/>
 						<Star fill="gold" color="gold"/>
 					</div>
-					<div className="text-xl">
-						(1.5k Reviews)
-					</div>
+					<a className="hover:underline" href="https://www.google.com/search?client=firefox-b-d&hs=PAaU&sca_esv=a675286474cf8d02&sxsrf=ANbL-n72185ypnlz2ktXPBg_Ho2-ml0pXA:1769624036299&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOftKwOFTxOwX9A0qssmO66ufnG5FKTidiMwIPKeygXOO1NVO6SAAJiWz-EFfMY1U0vsWCiYptAB2u40tQk7AuKG6DNobDe9t_ne5FM3Ec94VYcRHTw%3D%3D&q=Fatto+a+Mano+Covent+Garden+Reviews&sa=X&ved=2ahUKEwjcgseg666SAxUKU0EAHZ4PJkMQ0bkNegQIThAH&biw=1464&bih=1186&dpr=1&aic=0">
+						<div className="text-xl">
+							(1.5k Reviews)
+						</div>
+					</a>
 				</div>
 			</CardTitle>
 		  <CardDescription className="text-2xl font-semibold text-left">Saturday 31st Jan</CardDescription>
